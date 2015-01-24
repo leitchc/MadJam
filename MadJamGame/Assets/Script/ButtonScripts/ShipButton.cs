572592f110f8302c9ba1bc0231b6b2379f754d60
leitchc <colin.andrew.leitch@gmail.com>
2015-01-24 16:13:42 -0700

    Fire button works

--- MadJamGame/Assets/Script/ButtonScripts/ShipButton.cs
+++ MadJamGame/Assets/Script/ButtonScripts/ShipButton.cs
@@ -4,14 +4,34 @@ using System.Collections;
 public class ShipButton : MonoBehaviour {
 
     public AudioClip buttonSound;
-
+    public Hazard hazard;
+    public string b_name;
+    
+    public bool state = false;
+    public float pressedTimer = 0f;
+    
 	// Use this for initialization
 	void Start () {
-	
+		HazardChecker.Instance.AddButton(b_name, hazard.h_name);
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Update(){
+		if(!state)return;
+		pressedTimer += Time.deltaTime;
+		if(pressedTimer >= 0.1f){
+			HazardChecker.Instance.SetButtonState(b_name, false);
+			state = false;
+			pressedTimer = 0f;
+		}
+	}
 	
+	public void Clicked(){
+		if(state)return;
+		
+		state = true;
+		if(hazard.isActive){
+			hazard.isActive = false;
+			HazardChecker.Instance.SetHazardState(hazard.h_name, false);
+		}
 	}
 }
