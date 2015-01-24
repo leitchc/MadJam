086d46f7b3010bd21e2b3662b4049290d3d389db
Thomas Curnow <curnow@ualberta.ca>
2015-01-24 15:33:18 -0700

    added button scrip skeleton

---
+++ MadJamGame/Assets/Script/ButtonScripts/ShipButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipButton : MonoBehaviour {
+
+    public AudioClip buttonSound;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
