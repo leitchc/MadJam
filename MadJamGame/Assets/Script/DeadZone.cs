9a93c9a4959f428fbcef4dba707e2999704388a8
Eric Lam <elam3@ualberta.ca>
2015-01-24 09:56:08 -0700

    Worked on Asteroids.

---
+++ MadJamGame/Assets/Script/DeadZone.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+	Destroys anything that enters
+*/
+public class DeadZone : MonoBehaviour {
+
+
+	void OnTriggerEnter(Collider col) {
+
+	}
+}
---
+++ MadJamGame/Assets/Script/MoveTowards.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTowards : MonoBehaviour {
+
+	public Transform target;
+	public GameObject explosion;
+	public float speed = 5.0f;
+	
+	void Start() {
+		if(!target) {
+			target = GameObject.Find("Main Camera").transform;
+		}		
+	}
+
+	// Update is called once per frame
+	void Update () {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+		if(other.tag == "Player") {
+			if(explosion != null) {
+				Instantiate(explosion, transform.position, transform.rotation);
+			}
+			Destroy(gameObject);
+		}
+	}
+}
