24faf1d55694ca8f97fd77859e346005429586d1
Thomas Curnow <curnow@ualberta.ca>
2015-01-24 17:23:05 -0700

    implimented Kill yourself button

--- MadJamGame/Assets/Script/ButtonScripts/SelfDestructButton.cs
+++ MadJamGame/Assets/Script/ButtonScripts/SelfDestructButton.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class SelfDestructButton : ShipButton
 {
 
+    public AudioClip explosionButtonSound;
+    public AudioSource explosionSoundSource;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -13,4 +16,16 @@ public class SelfDestructButton : ShipButton
 	void Update () {
 	
 	}
+
+    public void Explode() {
+
+        //yield return new WaitForSeconds(2);
+
+        GameObject main = GameObject.Find("Main");
+        GuiManager guiManager = (GuiManager)main.GetComponent(typeof(GuiManager));
+        guiManager.PlayerDeath();
+
+        explosionSoundSource.clip = explosionButtonSound;
+        explosionSoundSource.Play();
+    }
 }
