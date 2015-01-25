43cb8eb0d58f443e205d66fde51d95fab642e54f
Eric Lam <elam3@ualberta.ca>
2015-01-24 17:56:44 -0700

    Alien Ship will destroy self when reaching its destination.

--- MadJamGame/Assets/Script/StartGame.cs
+++ MadJamGame/Assets/Script/StartGame.cs
@@ -3,12 +3,21 @@ using System.Collections;
 
 public class StartGame : MonoBehaviour {
 
+	private bool buttonPressed = false;
+
 	public void GameStart() {
 		StartCoroutine(TravelWormhole());
 	}
 
+	void Update() {
+		if(buttonPressed) {
+
+		}
+	}
+
 	IEnumerator TravelWormhole() {
 		yield return new WaitForSeconds(2.0f);
-		
+
+		Application.LoadLevel(1);
 	}
 }
