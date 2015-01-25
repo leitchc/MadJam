7a00bc4d66241ebcd2f3036ec8f68ddc5512388a
Eric Lam <elam3@ualberta.ca>
2015-01-25 13:46:12 -0700

    - Added Background Music - Added Timer Script

---
+++ MadJamGame/Assets/Script/UI/Timer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	public Text textUI;
+	
+	private float timer = 0;
+
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		string minutes = Mathf.Floor(timer / 60).ToString("00");
+ 		string seconds = Mathf.Floor(timer % 60).ToString("00");
+		textUI.text = minutes + ":" + seconds;
+	}
+}
