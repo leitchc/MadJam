e70b714e91ab7b5b796634223737e34e77530de9
Eric Lam <elam3@ualberta.ca>
2015-01-24 17:27:55 -0700

    - Added "Perfect Dark" font. - Worked on Menu scene - Added Scripts related to UI Buttons in Menu scene

---
+++ MadJamGame/Assets/Script/StartGame.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+	public void GameStart() {
+		StartCoroutine(TravelWormhole());
+	}
+
+	IEnumerator TravelWormhole() {
+		yield return new WaitForSeconds(2.0f);
+		
+	}
+}
---
+++ MadJamGame/Assets/Script/UI/ChangeView.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeView : MonoBehaviour {
+
+	public GameObject[] oldViews;
+	public GameObject[] newViews;
+
+	public void ViewChange() {
+		if(oldViews.Length > 0) {
+			for(int i = 0; i < oldViews.Length; i++) {
+				oldViews[i].SetActive(false);
+			}
+		}
+		if(newViews.Length > 0) {
+			for(int j = 0; j < newViews.Length; j++) {
+				newViews[j].SetActive(true);
+			}
+		}
+	}
+}
