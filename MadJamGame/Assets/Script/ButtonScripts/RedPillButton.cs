5bbc3af766deed92dbc034cbefb603a31dab7995
Eric Lam <elam3@ualberta.ca>
2015-01-25 15:56:44 -0700

    Red Pill Added.

---
+++ MadJamGame/Assets/Script/ButtonScripts/RedPillButton.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RedPillButton : ShipButton
+{
+    private bool isRunning = false;
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+
+    public void TakePill() {
+
+        //yield return new WaitForSeconds(2);
+
+        soundSource.clip = buttonSound;
+        soundSource.Play();
+
+        GameObject main = GameObject.Find("Main");
+
+        if(!isRunning) {
+            StartCoroutine(LoadMatrix());
+        }
+    }
+
+    IEnumerator LoadMatrix() {
+        isRunning = true;
+        yield return new WaitForSeconds(1.0f);
+        Application.LoadLevel("shipmatrix");
+    }
+}
