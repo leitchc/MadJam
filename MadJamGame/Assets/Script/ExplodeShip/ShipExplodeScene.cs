99f8652f4e5454c0a9869fe3966718ef8f02075f
Eric Lam <elam3@ualberta.ca>
2015-01-25 10:58:06 -0700

    Worked on 2 End Game Scenes: Explosion and Eject.

---
+++ MadJamGame/Assets/Script/ExplodeShip/ShipExplodeScene.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipExplodeScene : MonoBehaviour {
+
+	public Animator shipAnim;
+	public GameObject endUI;
+
+	// Use this for initialization
+	void Start () {
+		if(shipAnim) {
+			StartCoroutine(ShipExplosion());
+		}
+	}
+	
+	IEnumerator ShipExplosion() {
+		shipAnim.SetBool("Start", true);
+		yield return new WaitForSeconds(5.0f);
+
+		yield return new WaitForSeconds(2.0f);
+		
+		if(endUI) {
+			endUI.SetActive(true);
+		}
+
+	}
+}
---
+++ MadJamGame/Assets/Script/UI/LevelLoader.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour {
+	public string levelName;
+
+	public void LevelLoad() {
+		Application.LoadLevel(levelName);
+	}
+}
