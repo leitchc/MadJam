e251e89eca018e44a408172f742d2428c21b78b6
Eric Lam <elam3@ualberta.ca>
2015-01-25 15:48:54 -0700

    Linked shipeject scene to Eject Button.

---
+++ MadJamGame/Assets/Script/ButtonScripts/EjectButton.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EjectButton : ShipButton
+{
+    private bool isRunning = false;
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+
+    public void Eject() {
+
+        //yield return new WaitForSeconds(2);
+
+        soundSource.clip = buttonSound;
+        soundSource.Play();
+
+        GameObject main = GameObject.Find("Main");
+
+        if(!isRunning) {
+            StartCoroutine(LoadEject());
+        }
+    }
+
+    IEnumerator LoadEject() {
+        isRunning = true;
+        yield return new WaitForSeconds(1.0f);
+        Application.LoadLevel("shipeject");
+    }
+}
