c9b42a564ba65be52f6c249b35fd1a185d9298ff
Eric Lam <elam3@ualberta.ca>
2015-01-24 19:12:28 -0700

    Worked on short cutscene for Starting game.

--- MadJamGame/Assets/Script/StartGame.cs
+++ MadJamGame/Assets/Script/StartGame.cs
@@ -3,20 +3,36 @@ using System.Collections;
 
 public class StartGame : MonoBehaviour {
 
+	public Animator anim;
+	public ChangeAlpha fadeWhite;
+	public ChangeAlpha fadeBlack;
+	public GameObject tunnel;
+
 	private bool buttonPressed = false;
 
 	public void GameStart() {
 		StartCoroutine(TravelWormhole());
 	}
 
-	void Update() {
-		if(buttonPressed) {
-
-		}
-	}
-
 	IEnumerator TravelWormhole() {
+		// Play Animation
+		if(anim) {
+			anim.SetBool("Start", true);
+		}
 		yield return new WaitForSeconds(2.0f);
+		fadeWhite.ToOpaque();
+
+		yield return new WaitForSeconds(2.0f);
+
+		tunnel.SetActive(true);
+		fadeWhite.ToTransparent();
+
+		yield return new WaitForSeconds(4.0f);
+
+		fadeBlack.ToOpaque();
+
+		yield return new WaitForSeconds(2.0f);
+
 
 		Application.LoadLevel(1);
 	}
