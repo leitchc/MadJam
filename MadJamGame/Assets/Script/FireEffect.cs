ed3fcd9b63694e1015d838d02df17ac36d55df2c
Thomas Curnow <curnow@ualberta.ca>
2015-01-24 19:53:33 -0700

    fireeee

--- MadJamGame/Assets/Script/FireEffect.cs
+++ MadJamGame/Assets/Script/FireEffect.cs
@@ -1,31 +1,53 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using System.Collections;
 
-public class FireEffect : MonoBehaviour {
+public class FireEffect : MonoBehaviour
+{
 
-	public ParticleSystem particleSys;
-	//Location
-	public float xMin = 1.0f;
-	public float xMax = 10.0f;
-	public float yMin = -10.0f;
-	public float yMax = 0.0f;
-	//Scale
-	public float minScale = 0.02f;
-	public float maxScale = 0.05f;
+    public GameObject particleSys;
+    //Location
+    public float xMin = 0.0f;
+    public float xMax = 0.0f;
+    public float yMin = 0.0f;
+    public float yMax = 0.0f;
+    //Scale
+    public float minScale = 2.0f;//0.02f;
+    public float maxScale = -2.0f;//-0.03f;
+    private bool runOnce = false;
+    // Use this for initialization
+    void Start()
+    {
+        xMin = -particleSys.transform.localPosition.x;
+        xMax = particleSys.transform.localPosition.x;
+        yMin = particleSys.transform.localPosition.y;
+    }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(HazardChecker.Instance.GetHazardState("Fire Detected") == 1) {
-			//particleSys.transform.localPosition.Set(Random.Range(xMin, xMax),Random.Range(yMin, yMax),0);
-			particleSys.transform.localScale.Set(Random.Range(minScale, maxScale),Random.Range(minScale, maxScale), Random.Range(minScale, maxScale));
-			particleSys.enableEmission = true;
-		} else {
-			particleSys.enableEmission = false;
-		}
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (HazardChecker.Instance.GetHazardState("Fire Detected") == 1)
+        {
+            if (!runOnce)
+            {
+                particleSys.transform.localPosition = new Vector3(
+                        particleSys.transform.localPosition.x - Random.Range(xMin, xMax),
+                        particleSys.transform.localPosition.y - Random.Range(yMin, yMax),
+                        particleSys.transform.localPosition.z);
+
+                particleSys.transform.localScale = new Vector3(
+                        particleSys.transform.localScale.x - Random.Range(minScale, maxScale),
+                        particleSys.transform.localScale.y - Random.Range(minScale, maxScale),
+                        particleSys.transform.localScale.z - Random.Range(minScale, maxScale));
+                particleSys.particleSystem.enableEmission = true;
+                runOnce = true;
+            }
+        }
+        else
+        {
+            particleSys.particleSystem.enableEmission = false;
+            runOnce = false;
+        }
+    }
 }
+
