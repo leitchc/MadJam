78012b6a579a99a5ade85de00f66ba1eabbb75d9
leitchc <colin.andrew.leitch@gmail.com>
2015-01-25 10:13:34 -0700

    Transition from title to game, and restart/quit cleanup

--- MadJamGame/Assets/Script/GuiManager.cs
+++ MadJamGame/Assets/Script/GuiManager.cs
@@ -3,10 +3,8 @@ using System.Collections;
 
 public class GuiManager : MonoBehaviour {
 	
-	public GameObject TitleScreen;
 	public GameObject GameScreen;
 	public GameObject DeathScreen;
-	public GameObject TutorialScreen;
 	
 	private static GuiManager instance = null;
 	public static GuiManager Instance {get {return instance; }}
@@ -17,25 +15,8 @@ public class GuiManager : MonoBehaviour {
 	}
 	
 	void Start(){
-		if(TitleScreen != null)TitleScreen.SetActive(true);
-		if(GameScreen != null)GameScreen.SetActive(false);
+		if(GameScreen != null)GameScreen.SetActive(true);
 		if(DeathScreen != null)DeathScreen.SetActive(false);
-		if(TutorialScreen != null)TutorialScreen.SetActive(false);
-	}
-	
-	public void OnPressPlay(){
-		TitleScreen.SetActive(false);
-		//TutorialScreen.SetActive(true);
-		GameScreen.SetActive(true);
-	}
-	
-	public void OnEnterGameplay(){
-		TutorialScreen.SetActive(false);
-	}
-	
-	public void DebugDie(){
-		//GameScreen.SetActive(false);
-		DeathScreen.SetActive(true);
 	}
 	
 	public void PlayerDeath(){
@@ -43,17 +24,10 @@ public class GuiManager : MonoBehaviour {
 	}
 	
 	public void Restart(){
-		DeathScreen.SetActive(false);
-		Ship tmp = GameObject.FindGameObjectWithTag("Player").GetComponent<Ship>();
-		tmp.HPRefil();
-		//GameScreen.SetActive(true);
+		Application.LoadLevel(1);
 	}
 	
 	public void Quit(){
-		DeathScreen.SetActive(false);
-		GameScreen.SetActive(false);
-		TitleScreen.SetActive(true);
-		Ship tmp = GameObject.FindGameObjectWithTag("Player").GetComponent<Ship>();
-		tmp.HPRefil();
+		Application.LoadLevel(0);
 	}
 }
