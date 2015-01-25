e4903a57a15cfcd729453a0f7cb2ffb98cf2d96e
Thomas Curnow <curnow@ualberta.ca>
2015-01-24 19:39:17 -0700

    Door button!

---
+++ MadJamGame/Assets/Script/ButtonScripts/DoorButton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorButton : ShipButton {
+
+    public AudioClip doorClosedSound;
+    public AudioClip doorOpenSound;
+    public AudioSource doorSoundSource;
+
+    public bool doorClosed = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OpenDoor() {
+        if (doorClosed == false) {
+            doorSoundSource.clip = doorOpenSound;
+            doorSoundSource.Play();
+            doorClosed = true;
+        }
+        else{
+            doorSoundSource.clip = doorClosedSound;
+            doorSoundSource.Play();
+            doorClosed = false;
+        }
+    }
+}
