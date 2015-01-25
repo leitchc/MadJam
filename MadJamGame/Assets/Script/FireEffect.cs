47b4f2381814c53a5505a9502a2bed0b10dc5403
Eric Lam <elam3@ualberta.ca>
2015-01-25 14:30:38 -0700

    Added my Timer to scene1 and Ryan's FireEffect modifications.

--- MadJamGame/Assets/Script/FireEffect.cs
+++ MadJamGame/Assets/Script/FireEffect.cs
@@ -6,21 +6,28 @@ public class FireEffect : MonoBehaviour
 {
 
     public GameObject particleSys;
+    // horizontal screen: -0.46  to 0.46
+    // vertical screen is: -0.19 to 0.27
+    // spaceship's window is at 0.03 to 0.18
     //Location
-    public float xMin = 0.0f;
-    public float xMax = 0.0f;
-    public float yMin = 0.0f;
-    public float yMax = 0.0f;
+    public float xMin = -0.46f;
+    public float xMax = 0.46f;
+    public float yMin = -0.19f;
+    public float yMax = 0.27f;
+    //Window vertical location
+    public float winMin = 0.03f;
+    public float winMax = 0.18f;
     //Scale
-    public float minScale = 2.0f;//0.02f;
-    public float maxScale = -2.0f;//-0.03f;
+    public float minScale = 0.01f;
+    public float maxScale = 0.12f;
+    
     private bool runOnce = false;
+    private static float lastTime = 0.0f;
+    public float timeForFireGrow = 5.0f;
+    
     // Use this for initialization
     void Start()
     {
-        xMin = -particleSys.transform.localPosition.x;
-        xMax = particleSys.transform.localPosition.x;
-        yMin = particleSys.transform.localPosition.y;
     }
 
     // Update is called once per frame
@@ -30,23 +37,39 @@ public class FireEffect : MonoBehaviour
         {
             if (!runOnce)
             {
-                particleSys.transform.localPosition = new Vector3(
-                        particleSys.transform.localPosition.x - Random.Range(xMin, xMax),
-                        particleSys.transform.localPosition.y - Random.Range(yMin, yMax),
-                        particleSys.transform.localPosition.z);
+                float yLoc = 0.0f;
+                while((yLoc > winMin) && (yLoc < winMax))
+                    yLoc = Random.Range(yMin, yMax);
 
-                particleSys.transform.localScale = new Vector3(
-                        particleSys.transform.localScale.x - Random.Range(minScale, maxScale),
-                        particleSys.transform.localScale.y - Random.Range(minScale, maxScale),
-                        particleSys.transform.localScale.z - Random.Range(minScale, maxScale));
+                particleSys.transform.localPosition = new Vector3(
+                    Random.Range(xMin, xMax), yLoc, particleSys.transform.localPosition.z);
+
+                particleSys.transform.localScale = new Vector3( 
+                    Random.Range(minScale, maxScale),
+                    Random.Range(minScale, maxScale),
+                    Random.Range(minScale, maxScale));
+                particleSys.particleSystem.Play();    
                 particleSys.particleSystem.enableEmission = true;
                 runOnce = true;
             }
         }
         else
         {
+            particleSys.particleSystem.Stop();
             particleSys.particleSystem.enableEmission = false;
             runOnce = false;
+        }
+
+        if(((lastTime + timeForFireGrow) <= Time.time) 
+           && (particleSys.transform.localScale.x < particleSys.transform.localScale.x*5.0f) 
+           && (particleSys.transform.localScale.y < particleSys.transform.localScale.y*5.0f)
+           && (particleSys.transform.localScale.z < particleSys.transform.localScale.z*5.0f)
+           ) {
+            particleSys.transform.localScale = new Vector3( 
+               particleSys.transform.localScale.x*1.15f,
+               particleSys.transform.localScale.y*1.15f,
+               particleSys.transform.localScale.z*1.15f);
+            lastTime = Time.time;
         }
     }
 }
